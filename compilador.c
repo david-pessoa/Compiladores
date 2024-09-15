fd9b55f7c056427c41a8d658be6cb132bbe4ff7e
david-pessoa <pessoa.dvlbp@gmail.com>
2024-09-15 19:16:41 -0300

    add mini analisador léxico

--- compilador.c
+++ compilador.c
@@ -2,7 +2,30 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <ctype.h>
 
+typedef enum{ //tipo dos átomos
+    IDENTIFICADOR,
+    NUMERO,
+    ERRO,
+    EOF_BUFFER,
+    COMENTARIO
+}Atomo;
+
+typedef struct{
+    Atomo atomo;
+    int linha;
+    float atributo_numero;
+    char atributo_ID[16];
+}InfoAtomo;
+
+char *msgAtomo[] = {
+    "IDENTIFICADOR",
+    "NUMERO",
+    "ERRO",
+    "EOF_BUFFER",
+    "COMENTARIO"
+};
 
 //Só existem tipo bool e int
 //Função obter_atomo() --> analisador léxico
@@ -10,7 +33,7 @@
 //Não existe else nem while (pelo que entendi)
 
 //consome() possui obter_atomo() em seu interior (faz chamadas)
-//Ignorar caracteres (‘ ’, ‘\n’, ‘\t, ‘\r’) OBS: usar '\n' para fazer contagem de linhas
+//Ignorar caracteres (‘ ’, ‘\n’, ‘\t, ‘\r’) OBS: usar '\n' para fazer contagem de linhas 
 
 //Comentários de uma linha: do '#' até o '\n'
 //Comentários de mais de uma linha: do '{-' até o '-}'
@@ -48,10 +71,56 @@
 //7) Interação com o sistema de arquivos
 //8) Controle da numeração de linhas do programa fonte
 
-int main(int argc, char *argv[])
-{
-    char* nome_arq = argv[1]; //executar como: ./compilador entrada.txt
+int conta_linha = 1; // conta as linhas do arquivo
 
+InfoAtomo obter_atomo(char *buffer){
+    InfoAtomo info_atomo;
+
+    // consome espaços em branco quebra de linhas tabulação e retorno de carro
+    while(*buffer == ' ' || *buffer == '\n' || *buffer == '\t' ||*buffer == '\r'){
+        if( *buffer =='\n')
+            conta_linha++;
+        buffer++;
+    }
+    // reconhece identificador
+    if( islower(*buffer)){ // ser for letra mininuscula
+        info_atomo = reconhece_identificador(buffer);
+    }else if(*buffer == 0){
+        info_atomo.atomo = EOF_BUFFER;
+    }else{
+        info_atomo.atomo = ERRO;
+    }
+    info_atomo.linha = conta_linha;
+    return info_atomo;
+
+}
+// IDENTIFICADOR -> LETRA_MINUSCULA (LETRA_MINUSCULA | DIGITO )*
+InfoAtomo reconhece_identificador(char *buffer){
+    InfoAtomo info_atomo;
+    info_atomo.atomo = ERRO;
+    char *iniID = buffer;
+    // ja temos uma letra minuscula
+    buffer++;
+
+q1:
+    if( islower(*buffer) || isdigit(*buffer)){
+        buffer++;
+        goto q1;
+    }
+    if( isupper(*buffer))
+        return info_atomo;
+
+    //"  var1@"
+    // falta testar o tamanho do identificador que dever ser menor que 15,
+    // se for maior eh erro lexico
+    strncpy(info_atomo.atributo_ID,iniID,buffer-iniID);
+    info_atomo.atributo_ID[buffer-iniID] = 0; // finaliza a string
+    info_atomo.atomo = IDENTIFICADOR;
+    return info_atomo;
+}
+
+char* le_arquivo(char* nome_arq)
+{
     FILE *file;
     char *buffer;
     long file_size;
@@ -81,12 +150,42 @@ int main(int argc, char *argv[])
     // Lê o conteúdo do arquivo e coloca na string `buffer`
     fread(buffer, 1, file_size, file);
     buffer[file_size] = '\0'; // Adiciona o terminador nulo ao final da string
+    fclose(file);
 
+    return buffer;
+}
+
+
+
+int main(int argc, char *argv[])
+{
+    char* nome_arq = argv[1]; //executar como: ./compilador entrada.txt
+    char* buffer = le_arquivo(nome_arq);
     
 
+    obter_atomo(buffer);
+
+    InfoAtomo info_atomo;
+    do{
+        // falta implementar o reconhecimento do atomo NUMERO
+        info_atomo = obter_atomo(buffer);
+        if( info_atomo.atomo == IDENTIFICADOR)
+            printf("%03d# %s | %s\n",info_atomo.linha,msgAtomo[info_atomo.atomo], info_atomo.atributo_ID);
+        else
+            printf("%03d# %s\n",info_atomo.linha,msgAtomo[info_atomo.atomo]);
+
+    }while(info_atomo.atomo != EOF_BUFFER && info_atomo.atomo != ERRO);
+    printf("fim da analise lexica\n");
+    return 0;
+    
+
+
+
+
+
     // Libera memória e fecha o arquivo
-    free(buffer);
-    fclose(file);
+
+    
 
     return 0;
 }
